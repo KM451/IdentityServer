cbd7867a8c0abe3805375183ec285afb2f58e8ca
KM451 <kalinowski.mich@gmail.com>
2023-07-14 22:15:41 +0200

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,53 @@
+﻿using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource(name: "user", userClaims: new[] {JwtClaimTypes.Email})
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("scope1"),
+            new ApiScope("scope2"),
+            new ApiScope("api1")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+               {
+                   ClientId = "client",
+                   ClientName = "Client for Postman user",
+                   AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                   ClientSecrets = {new Secret("secret".Sha256())},
+                   AllowedScopes = {"api1", "user"},
+                   AlwaysSendClientClaims = true,
+                   AlwaysIncludeUserClaimsInIdToken = true,
+                   AllowAccessTokensViaBrowser = true,
+               },
+
+               new Client
+               {
+                   ClientId = "swagger",
+                   ClientName = "Client for Swagger user",
+                   AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                   ClientSecrets = {new Secret("secret".Sha256())},
+                   AllowedScopes = {"api1", "user"},
+                   AlwaysSendClientClaims = true,
+                   AlwaysIncludeUserClaimsInIdToken = true,
+                   AllowAccessTokensViaBrowser = true,
+                   RedirectUris = { "https://localhost:7063/swagger/oauth2-redirect.html" },
+                   AllowedCorsOrigins = { "https://localhost:7063" }
+               }
+        };
+}
